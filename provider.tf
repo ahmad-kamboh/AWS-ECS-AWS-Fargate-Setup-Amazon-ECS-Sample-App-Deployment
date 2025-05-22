3d6e48d515a0b7dc28e404998b0abd8c00d4104b
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-22 02:35:22 -0700

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "ecs-fargate/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
