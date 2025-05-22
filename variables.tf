1918befb622fc9a43f84245e0ecdc20743f1d263
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-22 02:35:41 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "ecs_task_execution_role_arn" {
+  description = "IAM role ARN for ECS task execution"
+  type        = string
+}
