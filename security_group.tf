90131e4faf855a53bbf5dd0c182305582bc88ae3
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-22 02:32:34 -0700

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-sg"
+  description = "Allow SSH, HTTP, and outbound"
+  vpc_id      = aws_vpc.ecs_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
